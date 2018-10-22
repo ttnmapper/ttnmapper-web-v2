1ec23f00cf26d5d298e43ab6637ee0d4108df696
Kolijn <kolijn.wolfaardt@gmail.com>
2018-10-22 07:39:20 +0200

    Add circle rendering for packets

--- src/components/Home/PacketRendering/PacketRendering.jsx
+++ src/components/Home/PacketRendering/PacketRendering.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Popup, CircleMarker, GeoJSON } from 'react-leaflet'
 
 import {fetchPacketData} from '../../../actions/map-events'
+
 
 class _PacketRendering extends Component {
 
@@ -17,20 +19,90 @@ class _PacketRendering extends Component {
 
   componentDidMount() {
     //Check if our settings match the state settings
-    if (this.props.requestedPackets !== this.props.statePackets) {
-      console.log("Data not matching, requesting!")
-      this.props.fetchPackets(this.props.requestedPackets.deviceID, this.props.requestedPackets.fromDate, this.props.requestedPackets.toDate)
+    if (this.props.requestedPackets) {
+      if (this.props.requestedPackets !== this.props.statePackets) {
+        console.log("Data not matching, requesting!")
+        this.props.fetchPackets(this.props.requestedPackets.deviceID, this.props.requestedPackets.fromDate, this.props.requestedPackets.toDate)
+      }
     }
   }
 
+  renderPacket(packet) {
+    let color = '#0000ff'
+    let rssi = parseFloat(packet.rssi)
+    let snr = parseFloat(packet.snr)
+
+    if (snr < 0) {
+      rssi = rssi + snr
+    }
+    if (rssi == 0) {
+      color = "black"
+    }
+    else if (rssi < -120) {
+      color = "blue"
+    }
+    else if (rssi < -115) {
+      color = "cyan"
+    }
+    else if (rssi < -110) {
+      color = "green"
+    }
+    else if (rssi < -105) {
+      color = "yellow"
+    }
+    else if (rssi < -100) {
+      color = "orange"
+    }
+    else {
+      color = "red"
+    }
+
+    const lineOptions = {
+      radius: 10,
+      color: color,
+      fillColor: color,
+      opacity: 0.3,
+      weight: 2
+    }
+
+    const markerOptions = {
+        stroke: false,
+        radius: 5,
+        color: color,
+        fillColor: color,
+        fillOpacity: 0.8
+    };
+
+    const distance = 5
+
+    return (
+      <CircleMarker center={[packet.lat, packet.lon]} {...markerOptions} key={"marker_" + packet.id}>
+        <Popup>
+          {packet.time}
+          <br />
+          <b>Node:</b> {packet.nodeaddr}<br />
+          <b>Received by gateway:</b> <br />{packet.gwaddr}<br />
+          <b>Location accuracy:</b> {packet.accuracy}<br />
+          <b>Packet id:</b> {packet.id}<br />
+          <b>RSSI:</b> {packet.rssi}dBm<br />
+          <b>SNR:</b> {packet.snr}dB<br />
+          <b>Link cost:</b> {rssi*-1}dB<br />
+          <b>DR:</b> {packet.datarate}<br />
+          <b>Distance:</b> {distance}m<br />
+          <b>Altitude: </b> {packet.alt}m <br/>
+        </Popup>
+        {/* <Polyline positions={[[packet.lat, packet.lon],[]]}/> */}
+      </CircleMarker>
+    )
+  }
+
   render() {
-    // if ('mode' in this.params && this.params.mode === 'packets') {
-    //   console.log("Rendering packets")
-    //   return ("Packets")
-    // }
-    // else {
-      return (" ")
-    // }
+    if (this.props.requestedPackets) {
+      if (this.props.statePackets.packetsData.length > 0) {
+        return this.props.statePackets.packetsData.map(this.renderPacket);
+      }
+    }
+    return (" ")
   }
 }
 
