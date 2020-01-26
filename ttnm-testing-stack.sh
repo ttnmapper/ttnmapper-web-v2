bf98a68e0c862f28772c537e8af37d5c0d4836d2
Kolijn <kolijn.wolfaardt@gmail.com>
2020-01-26 12:56:01 +0200

    Updates script to run stack

--- ttnm-testing-stack.sh
+++ ttnm-testing-stack.sh
@@ -67,7 +67,8 @@ if [ $COMMAND = init ];  then
 	if ! [ -d "./certs" ]; then
 		mkdir certs
 		cd certs
-		go run $(go env GOROOT)/src/crypto/tls/generate_cert.go -ca -host localhost
+		openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out cert.pem -keyout key.pem
+		#openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
 		cp cert.pem key.pem ../
 		cd ..
 		sudo chown 886:886 ./cert.pem ./key.pem
@@ -83,6 +84,7 @@ if [ $COMMAND = init ];  then
 		docker-compose run --rm stack is-db create-admin-user --id admin --email admin@localhost
 		# Create the client for the cli
 		docker-compose run --rm stack is-db create-oauth-client --id cli --name "Command Line Interface" --owner admin --no-secret --redirect-uri 'local-callback' --redirect-uri 'code'
+		docker-compose run --rm stack is-db create-oauth-client --id console --name "Console" --owner admin --secret console --redirect-uri 'https://localhost:8885/console/oauth/callback'  --redirect-uri 'http://localhost:1885/console/oauth/callback' --redirect-uri '/console/oauth/callback'
 		touch docker_done
 	else
 		echo "Docker setup already done"
@@ -102,10 +104,12 @@ if [ $COMMAND = init ];  then
 	# 15 = RIGHT_APPLICATION_INFO, 20 = RIGHT_APPLICATION_DEVICES_READ,
 	# 30 = RIGHT_GATEWAY_INFO, 38 = RIGHT_GATEWAY_STATUS_READ, 39 = RIGHT_GATEWAY_LOCATION_READ
 	ttn-lw-cli --ca ./certs/cert.pem clients create --client-id ttnmapper --name "TTN Mapper" --user-id admin \
-	--redirect-uris "localhost:8010/login_callback" --secret="supasecret" --grants 0,2 --rights 1,6,8,15,20,30,38,39 --state STATE_APPROVED
+	--redirect-uris "localhost:8010/login_callback" --secret="supasecret" --grants GRANT_AUTHORIZATION_CODE,GRANT_REFRESH_TOKEN \
+	#--rights RIGHT_APPLICATION_ALL,RIGHT_APPLICATION_INFO,RIGHT_ORGANIZATION_APPLICATIONS_LIST,RIGHT_USER_APPLICATIONS_LIST \
+	--rights RIGHT_USER_INFO,RIGHT_USER_APPLICATIONS_LIST,RIGHT_USER_GATEWAYS_LIST,RIGHT_APPLICATION_INFO,RIGHT_APPLICATION_DEVICES_READ,RIGHT_GATEWAY_INFO,RIGHT_GATEWAY_STATUS_READ,RIGHT_GATEWAY_LOCATION_READ \
+	--state STATE_APPROVED
 
 	# Create users without being logged in due to https://github.com/TheThingsNetwork/lorawan-stack/pull/387, https://github.com/TheThingsNetwork/lorawan-stack/issues/672
-	ttn-lw-cli --ca ./certs/cert.pem logout
 	ttn-lw-cli --ca ./certs/cert.pem users create --user-id uid-allan --primary-email-address allan@localhost --name Allan --password AllanAllan1
 	ttn-lw-cli --ca ./certs/cert.pem users create --user-id uid-belinda --primary-email-address belinda@localhost --name Belinda --password BelindaBelinda1
 	ttn-lw-cli --ca ./certs/cert.pem users create --user-id uid-charlie --primary-email-address charlie@localhost --name "Charles Brown, Esq" --password CharlieCharlie1
@@ -115,7 +119,17 @@ if [ $COMMAND = init ];  then
 	ttn-lw-cli --ca ./certs/cert.pem users create --user-id uid-giselle --primary-email-address giselle@localhost --name Giselle --password GiselleGiselle1
 	ttn-lw-cli --ca ./certs/cert.pem users create --user-id uid-howard --primary-email-address howard@localhost --name Howard --password HowardHoward1
 	ttn-lw-cli --ca ./certs/cert.pem users create --user-id uid-ivan --primary-email-address ivan@localhost --name Ivan --password InvalidInvalid1
-	ttn-lw-cli --ca ./certs/cert.pem login 
+	
+	# Required to bypass password validation
+	ttn-lw-cli --ca ./certs/cert.pem user set uid-allan --state=STATE_APPROVED 
+	ttn-lw-cli --ca ./certs/cert.pem user set uid-belinda --state=STATE_APPROVED
+	ttn-lw-cli --ca ./certs/cert.pem user set uid-charlie --state=STATE_APPROVED
+	ttn-lw-cli --ca ./certs/cert.pem user set uid-diane --state=STATE_APPROVED
+	ttn-lw-cli --ca ./certs/cert.pem user set uid-edgar --state=STATE_APPROVED
+	ttn-lw-cli --ca ./certs/cert.pem user set uid-freddie --state=STATE_APPROVED
+	ttn-lw-cli --ca ./certs/cert.pem user set uid-giselle --state=STATE_APPROVED
+	ttn-lw-cli --ca ./certs/cert.pem user set uid-howard --state=STATE_APPROVED
+	ttn-lw-cli --ca ./certs/cert.pem user set uid-ivan --state=STATE_APPROVED
 
 	# Create some users
 	# User Allan,   - 3 applications, 2,3,4 devices each. Used for normal testing
@@ -129,17 +143,18 @@ if [ $COMMAND = init ];  then
 	# User Invalid 	- Nothing. Cancelles login due to wrong password
 
 	# User Allan,   - 3 applications, 2,3,4 devices each. Used for normal testing
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-1 --device-id dev-allan-1 --description "Allan device 1 description" --name "Allan device 1" --dev-eui 0111223344556601 --join-eui 8000000000010001 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-1 --device-id dev-allan-2 --description "Allan device 2 description" --name "Allan device 2" --dev-eui 0111223344556602 --join-eui 8000000000010002 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
+	ttn-lw-cli --ca ./certs/cert.pem application create --application-id app-allan-1 --name "Allan Application 1" --user-id uid-allan
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-1 --device-id dev-allan-1 --description "Allan device 1 description" --name "Allan device 1" --dev-eui 0111223344556601 --join-eui 8000000000010001 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-1 --device-id dev-allan-2 --description "Allan device 2 description" --name "Allan device 2" --dev-eui 0111223344556602 --join-eui 8000000000010002 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
 	ttn-lw-cli --ca ./certs/cert.pem application create --application-id app-allan-2 --name "Allan Application 2" --user-id uid-allan
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-2 --device-id dev-allan-3 --description "Allan device 3 description" --name "Allan device 3" --dev-eui 0111223344556603 --join-eui 8000000000010003 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-2 --device-id dev-allan-4 --description "Allan device 4 description" --name "Allan device 4" --dev-eui 0111223344556604 --join-eui 8000000000010004 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-2 --device-id dev-allan-5 --description "Allan device 5 description" --name "Allan device 5" --dev-eui 0111223344556605 --join-eui 8000000000010005 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-2 --device-id dev-allan-3 --description "Allan device 3 description" --name "Allan device 3" --dev-eui 0111223344556603 --join-eui 8000000000010003 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-2 --device-id dev-allan-4 --description "Allan device 4 description" --name "Allan device 4" --dev-eui 0111223344556604 --join-eui 8000000000010004 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-2 --device-id dev-allan-5 --description "Allan device 5 description" --name "Allan device 5" --dev-eui 0111223344556605 --join-eui 8000000000010005 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
 	ttn-lw-cli --ca ./certs/cert.pem application create --application-id app-allan-3 --name "Allan Application 3" --user-id uid-allan
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-3 --device-id dev-allan-6 --description "Allan device 6 description" --name "Allan device 6" --dev-eui 0111223344556606 --join-eui 8000000000010006 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-3 --device-id dev-allan-7 --description "Allan device 7 description" --name "Allan device 7" --dev-eui 0111223344556607 --join-eui 8000000000010007 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-3 --device-id dev-allan-8 --description "Allan device 8 description" --name "Allan device 8" --dev-eui 0111223344556608 --join-eui 8000000000010008 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-3 --device-id dev-allan-9 --description "Allan device 9 description" --name "Allan device 9" --dev-eui 0111223344556609 --join-eui 8000000000010009 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-3 --device-id dev-allan-6 --description "Allan device 6 description" --name "Allan device 6" --dev-eui 0111223344556606 --join-eui 8000000000010006 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-3 --device-id dev-allan-7 --description "Allan device 7 description" --name "Allan device 7" --dev-eui 0111223344556607 --join-eui 8000000000010007 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-3 --device-id dev-allan-8 --description "Allan device 8 description" --name "Allan device 8" --dev-eui 0111223344556608 --join-eui 8000000000010008 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-allan-3 --device-id dev-allan-9 --description "Allan device 9 description" --name "Allan device 9" --dev-eui 0111223344556609 --join-eui 8000000000010009 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
 
 
 	# User Belinda 	- 1 Application, 50 devices Testing Large number of devices
@@ -147,7 +162,8 @@ if [ $COMMAND = init ];  then
 	# Create 40 devices for this application
 	for i in $(seq -f "%02g" 1 40)
 	do
-		ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-belinda-1 --device-id "dev-belinda-$i" --description "Belinda device $i description" --name "Belinda device $i" --dev-eui "02112233445566$i" --join-eui "80000000000200$i" --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
+		ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-belinda-1 --device-id "dev-belinda-$i" --description "Belinda device $i description" --name "Belinda device $i" --dev-eui "02112233445566$i" --join-eui "80000000000200$i" --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
+
 	done
 
 
@@ -158,7 +174,8 @@ if [ $COMMAND = init ];  then
 	for i in $(seq -f "%02g" 1 40)
 	do
 		ttn-lw-cli --ca ./certs/cert.pem application create --application-id "app-diane-$i" --name "Diane Application $i" --user-id uid-diane
-		ttn-lw-cli --ca ./certs/cert.pem device create --application-id "app-diane-$i" --device-id "dev-diane-$i" --description "Diane device $i description" --name "Diane device $i" --dev-eui "04112233445566$i" --join-eui "80000000000400$i" --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
+		ttn-lw-cli --ca ./certs/cert.pem device create --application-id "app-diane-$i" --device-id "dev-diane-$i" --description "Diane device $i description" --name "Diane device $i" --dev-eui "04112233445566$i" --join-eui "80000000000400$i" --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
+
 	done
 
 
@@ -172,8 +189,10 @@ if [ $COMMAND = init ];  then
 
 	# User Giselle 	- Application, 2 devices
 	ttn-lw-cli --ca ./certs/cert.pem application create --application-id app-giselle-1 --name "Giselle Application 1" --user-id uid-giselle
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-giselle-1 --device-id dev-giselle-1 --description "Giselle device 1 description" --name "Giselle device 9" --dev-eui 0711223344556601 --join-eui 8000000000070009 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
-	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-giselle-1 --device-id dev-giselle-2 --description "Giselle device 2 description" --name "Giselle device 9" --dev-eui 0711223344556602 --join-eui 8000000000070009 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_868_890
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-giselle-1 --device-id dev-giselle-1 --description "Giselle device 1 description" --name "Giselle device 9" --dev-eui 0711223344556601 --join-eui 8000000000070009 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
+
+	ttn-lw-cli --ca ./certs/cert.pem device create --application-id app-giselle-1 --device-id dev-giselle-2 --description "Giselle device 2 description" --name "Giselle device 9" --dev-eui 0711223344556602 --join-eui 8000000000070009 --lorawan-version 1.0.2 --lorawan-phy-version 1.0.2-b --frequency-plan-id EU_863_870
+
 
 
 	# User Howard - Shares application with Giselle
